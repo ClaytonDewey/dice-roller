3eb81b5df1aa6b78b06929c10fe0c84e71925fac
ClaytonDewey <clay@dryadmedia.com>
2021-10-28 11:41:02 -0500

    Emitting the number rolled...
    But I never use EventEmitters

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,14 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+    selector: 'app-root',
+    templateUrl: './app.component.html',
+    styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'dice-roller';
+    title = 'dice-roller';
+
+    dieRolled = (rollValue: number) => {
+        console.log(rollValue)
+    };
 }
